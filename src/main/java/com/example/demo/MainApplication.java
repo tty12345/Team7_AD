c1f4c6057758c584a96413afd17bf1eeefe4aabb
alvin158 <zhitat93@hotmail.com>
2021-08-06 10:53:46 +0800

    first commit

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MainApplication {
 
+	// alvin's changes
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
