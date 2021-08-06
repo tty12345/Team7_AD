e0bbe4fde2223a165fac8546f2d410917a3c98ab
Teck Yi <teckyi1993@gmail.com>
2021-08-06 10:58:08 +0800

    pushagain

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//testing
-//testing
+//pushagain
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class })
 public class MainApplication {
 
